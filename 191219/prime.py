69c6bcf5630828e02c3359f4642f0645ae1543ec
brandonkim0511 <52554860+brandonkim0511@users.noreply.github.com>
2019-12-19 17:04:09 +0900

    pn

--- 191219/prime.py
+++ 191219/prime.py
@@ -6,7 +6,8 @@ start = 2
 while start <= 100:
     divider = start - 1
     while divider > 1:
-        if (start % divider != 0) and (divider == 3):
+        if (start % divider != 0) and (divider == 2):
+            # break
             print(start)
         divider -= 1
     start += 1
